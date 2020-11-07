bbfa0c5602f18180facac58ce8ac5fefc7c2a33d
Vidya C <59405528+VidyaCKabber@users.noreply.github.com>
2020-11-07 23:02:09 +0530

    Create Pointers.cpp

---
+++ HackerRank/Pointers.cpp
@@ -0,0 +1,30 @@
+/*
+Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of a to their sum, and b to their absolute difference. There is no return value, and no return statement is needed.
+*a = *a + *b;
+*b = |*a - *b|
+*/
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int c = *a;
+    
+    *a = *a + *b;
+    *b = c - *b;
+    
+    if(*b < 0){
+        *b = *b * -1;
+    }
+      
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
